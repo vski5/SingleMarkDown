62b7062bef19177e59653981218d38a7593b49e0
vski5 <2643380185@qq.com>
2022-10-10 17:09:17 +0100

    一个根据输入内容修改label的小函数和两个值得注意的包使用方法。 	//自上而下按顺序展示，在容器中 	w.SetContent(container.NewVBox(lab, entryLine, btm)) 	//设置大小 	w.Resize(fyne.Size{Height: 500, Width: 500})

--- main.go
+++ main.go
@@ -1,17 +1,41 @@
 package main
 
 import (
+	"fyne.io/fyne/v2"
 	"fyne.io/fyne/v2/app"
+	"fyne.io/fyne/v2/container"
 	"fyne.io/fyne/v2/widget"
 )
+
+type myApp struct {
+	lab *widget.Label
+}
 
 func main() {
 	a := app.New()
 	w := a.NewWindow("创造一个window,此处是window的标题")
-	w.SetContent(widget.NewLabel("我是标签,属于widget(组件)"))
-	//w.ShowAndRun()
+	//w.SetContent(widget.NewLabel("我是标签,属于widget(组件)"))
+	var myapp myApp
+	lab, btm, entryLine := myapp.changeTitle()
+	//自上而下按顺序展示，在容器中
+	w.SetContent(container.NewVBox(lab, entryLine, btm))
+	w.Resize(fyne.Size{Height: 500, Width: 500})
+	//w.ShowAndRun() //程序在此停止，直到关闭再运行其他的。
 	//展示视窗
 	w.Show()
-	//运行app
+	//运行app //一个程序只能有一个运行。
 	a.Run()
+}
+func (myApp *myApp) changeTitle() (*widget.Label, *widget.Button, *widget.Entry) {
+
+	lab := widget.NewLabel("new label")
+	myApp.lab = lab
+
+	entryLine := widget.NewEntry()
+
+	btm := widget.NewButton("i'm button", func() {
+		myApp.lab.SetText(entryLine.Text)
+	})
+
+	return lab, btm, entryLine
 }
